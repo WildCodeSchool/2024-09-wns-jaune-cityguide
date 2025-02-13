349a4660d4a58b8d773a3907feb0b2dbd3fa0f33
bechir-elya <bechir.elya@gmail.com>
2025-02-13 15:33:06 +0100

    codegen set up

---
+++ frontend/src/libs/graphql/operations.ts
@@ -0,0 +1,13 @@
+import { gql } from "@apollo/client";
+
+
+export const GET_CITIES = gql`
+  query GetCities {
+    getCities {
+      id
+      name
+      postalCode
+      lattitiude
+      longitude
+    }
+}`;
