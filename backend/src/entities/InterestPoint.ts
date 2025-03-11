b9c8b2292edebc3bbfd4821c83258fa53652a771
Noëllie CHATAIN SIX <noellie.chatain@hotmail.com>
2025-03-11 16:13:37 +0100

    create 'InterestPoint' entity (without foreign keys)

---
+++ backend/src/entities/InterestPoint.ts
@@ -0,0 +1,50 @@
+import { Field, ObjectType } from "type-graphql";
+import { BaseEntity, Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+@ObjectType()
+export class InterestPoint extends BaseEntity {
+	@Field()
+	@PrimaryGeneratedColumn()
+	id!: string;
+
+	@Field()
+	@Column()
+	name!: string;
+
+	@Field()
+	@Column()
+	description!: string;
+
+	@Field()
+	@Column()
+	address!: string;
+
+	@Field()
+	@Column("float")
+	latitude!: number;
+
+	@Field()
+	@Column("float")
+	longitude!: number;
+
+	@Field()
+	@Column()
+	link_url!: string;
+
+	// TODO: add FKs: id_city and category_ic
+
+	// @Field(() => Category)
+	// @ManyToOne(
+	// 	() => Category,
+	// 	(category) => category.interestPoints,
+	// )
+	// id_category!: Category;
+
+	// @Field(() => City)
+	// @ManyToOne(
+	// 	() => City,
+	// 	(city) => category.interestPoints,
+	// )
+	// id_city!: City;
+}
