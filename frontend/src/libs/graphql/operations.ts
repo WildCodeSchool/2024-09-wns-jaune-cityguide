a0dd335bd64426b9fc6216a6cf407a73ace55213
Noëllie CHATAIN SIX <noellie.chatain@hotmail.com>
2025-04-09 11:46:54 +0200

    fix typo

--- frontend/src/libs/graphql/operations.ts
+++ frontend/src/libs/graphql/operations.ts
@@ -1,13 +1,12 @@
 import { gql } from "@apollo/client";
 
-
 export const GET_CITIES = gql`
   query GetCities {
     getCities {
       id
       name
       postalCode
-      lattitiude
+      lattitude
       longitude
     }
 }`;
