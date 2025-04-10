658202624db376607960dec81984f2ac76fd98dc
Noëllie CHATAIN SIX <noellie.chatain@hotmail.com>
2025-04-10 18:15:31 +0200

    fix components layout and style to prevent elements from overlapping

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,23 +3,22 @@ import "./App.css";
 import Footer from "./organisms/Footer";
 import Header from "./organisms/Header";
 import { useCitiesStore } from "./store/citiesStore";
+import { useInterestPointsStore } from "./store/interestPointsStore";
 import { useEffect } from "react";
 
 export function App() {
 	const { fetchCities } = useCitiesStore();
+	const { fetchInterestPoints } = useInterestPointsStore();
 
 	useEffect(() => {
 		fetchCities();
 		fetchInterestPoints();
-		fetchInterestPointsByCity("1");
-	}, [fetchCities, fetchInterestPoints, fetchInterestPointsByCity]);
+	}, [fetchCities, fetchInterestPoints]);
 
 	return (
-		<div className="flex flex-col min-h-screen">
+		<div className="flex flex-col min-h-screen max-h-screen overflow-y-hidden">
 			<Header />
-			<main className="flex-1">
-				<Outlet />
-			</main>
+			<Outlet />
 			<Footer />
 		</div>
 	);
