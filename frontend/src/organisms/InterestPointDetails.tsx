5bccebd041f5d1612a4925e54d629d3237a242a3
Noëllie CHATAIN SIX <noellie.chatain@hotmail.com>
2025-04-16 14:14:06 +0200

    display interest point details

--- frontend/src/organisms/InterestPointDetails.tsx
+++ frontend/src/organisms/InterestPointDetails.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useRef, useState } from "react";
 import type { InterestPoint } from "../@types/types";
 import { useInterestPointsStore } from "../store/interestPointsStore";
+import { Carousel } from "../atoms/Carousel";
 
 type InterestPointSheetProps = {
 	isOpen: boolean;
@@ -12,6 +13,8 @@ export default function InterestPointDetails({
 	isOpen,
 	onClose,
 }: InterestPointSheetProps) {
+	const { selectedInterestPoint } = useInterestPointsStore();
+
 	// Handling click outside of the sheet: https://dev.to/rashed_iqbal/how-to-handle-outside-clicks-in-react-with-typescript-4lmc
 	const sheetRef = useRef<HTMLDivElement>(null);
 
@@ -33,9 +36,9 @@ export default function InterestPointDetails({
 	return (
 		<>
 			<aside
-				className={`absolute top-0 right-0 h-full w-full sm:w-1/4 max-w-3xl bg-white text-black p-4 transform transition-transform duration-300 z-50 ${
+				className={`absolute top-0 right-0 h-full w-full sm:w-1/4 max-w-3xl bg-gray-50 text-black p-4 transform transition-transform duration-300 z-50 ${
 					isOpen ? "translate-x-0" : "translate-x-full"
-				} rounded-tl-xl rounded-bl-xl p-6 shadow-xl`}
+				} rounded-tl-xl rounded-bl-xl p-6 shadow-xl flex flex-col gap-4 content-center`}
 			>
 				<button
 					type="button"
@@ -61,7 +64,63 @@ export default function InterestPointDetails({
 					</svg>
 				</button>
 
-				<p className="text-black">Interest point details</p>
+				<div className="sheet-header w-full flex items-center justify-center space-x-4 py-4 text-gray-600">
+					<div className="h-[1.5px] w-full bg-gray-600 rounded-full flex-grow" />
+					<h3 className="text-2xl font-semibold whitespace-nowrap">
+						{selectedInterestPoint?.name}
+					</h3>
+					<div className="h-[1.5px] w-full  bg-gray-600 rounded-full flex-grow" />
+				</div>
+				<div className="carousel-container w-full flex justify-center">
+					<Carousel />
+				</div>
+				<div className="details-content space-y-4 text-sm sm:text-base">
+					{selectedInterestPoint?.description && (
+						<p className="text-gray-700 italic">
+							{selectedInterestPoint.description}
+						</p>
+					)}
+
+					{selectedInterestPoint?.address && (
+						<div>
+							<h4 className="font-semibold text-[#706eeb]">📍&nbsp;Adresse</h4>
+							<p>{selectedInterestPoint.address}</p>
+						</div>
+					)}
+
+					<div className="grid grid-cols-2 gap-4">
+						{selectedInterestPoint?.city && (
+							<div>
+								<h4 className="font-semibold text-[#706eeb]">🏙️&nbsp;Ville</h4>
+								<p>{selectedInterestPoint?.city.name}</p>
+							</div>
+						)}
+						{selectedInterestPoint?.category && (
+							<div>
+								<h4 className="font-semibold text-[#706eeb]">
+									📁&nbsp;Catégorie
+								</h4>
+								<p>{selectedInterestPoint.category.name}</p>
+							</div>
+						)}
+					</div>
+
+					{selectedInterestPoint?.link_url && (
+						<div>
+							<h4 className="font-semibold text-amber-600">
+								🔗&nbsp;Site officiel
+							</h4>
+							<a
+								href={selectedInterestPoint.link_url}
+								target="_blank"
+								rel="noopener noreferrer"
+								className="text-blue-600 underline hover:text-blue-800"
+							>
+								{selectedInterestPoint.link_url}
+							</a>
+						</div>
+					)}
+				</div>
 			</aside>
 		</>
 	);
