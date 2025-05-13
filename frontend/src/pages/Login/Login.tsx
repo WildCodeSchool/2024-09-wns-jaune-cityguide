f54742577ecbacdd11059e4efd69b0ce1a649cf0
bechir-elya <bechir.elya@gmail.com>
2025-05-13 15:50:17 +0200

    login page update

--- frontend/src/pages/Login/Login.tsx
+++ frontend/src/pages/Login/Login.tsx
@@ -3,112 +3,113 @@ import logo from "../../assets/logo.png";
 import connexion from "../../assets/connexion.png";
 import { type FormEvent, useState } from "react";
 import {
-  useLoginUserMutation,
-  type UserInput,
+	useLoginUserMutation,
+	type UserInput,
 } from "../../libs/graphql/generated/graphql-types";
 import { useUserStore } from "../../store/userStore";
 import background_form from "../../assets/background_form.png";
 
 const Login = () => {
-  const [login] = useLoginUserMutation();
-  const navigate = useNavigate();
-  const [message, setMessage] = useState<{ type: string; text: string } | null>(
-    null
-  );
-  const setUser = useUserStore((state) => state.setUser);
+	const [login] = useLoginUserMutation();
+	const navigate = useNavigate();
+	const [message, setMessage] = useState<{ type: string; text: string } | null>(
+		null
+	);
+	const setUser = useUserStore((state) => state.setUser);
 
-  const [showPassword, setShowPassword] = useState(false);
+	const [showPassword, setShowPassword] = useState(false);
 
-  const handleLogin = async (evt: FormEvent) => {
-    evt.preventDefault();
+	const handleLogin = async (evt: FormEvent) => {
+		evt.preventDefault();
 
-    const form = evt.target;
-    const formData = new FormData(form as HTMLFormElement);
-    const formJson = Object.fromEntries(formData.entries()) as UserInput;
+		const form = evt.target;
+		const formData = new FormData(form as HTMLFormElement);
+		const formJson = Object.fromEntries(formData.entries()) as UserInput;
 
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    if (!emailRegex.test(formJson.email)) {
-      setMessage({ type: "error", text: "Le format de l'email est invalide." });
-      return;
-    }
+		const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+		if (!emailRegex.test(formJson.email)) {
+			setMessage({ type: "error", text: "Le format de l'email est invalide." });
+			return;
+		}
 
-    try {
-      const { data } = await login({
-        variables: { data: formJson },
-      });
+		try {
+			const { data } = await login({
+				variables: { data: formJson },
+			});
 
-      if (data?.loginUser) {
-        const parsed = JSON.parse(data.loginUser);
+			if (data?.loginUser) {
+				const parsed = JSON.parse(data.loginUser);
 
-        setUser({
-          id: parsed.id,
-          firstname: parsed.firstname,
-          role: parsed.role,
-        });
+				setUser({
+					id: parsed.id,
+					firstname: parsed.firstname,
+					role: parsed.role,
+				});
 
-        setMessage({
-          type: "success",
-          text: "Connexion réussie ! Redirection...",
-        });
-        setTimeout(() => {
-          navigate("/");
-        }, 2000);
-      }
-    } catch (error: any) {
-      const code = error?.graphQLErrors?.[0]?.extensions?.code;
+				setMessage({
+					type: "success",
+					text: "Connexion réussie ! Redirection...",
+				});
+				setTimeout(() => {
+					navigate("/");
+				}, 2000);
+			}
+		} catch (error: any) {
+			const code = error?.graphQLErrors?.[0]?.extensions?.code;
 
-      switch (code) {
-        case "USER_NOT_FOUND":
-          setMessage({
-            type: "error",
-            text: "Le compte avec cet email n'existe pas.",
-          });
-          break;
+			switch (code) {
+				case "USER_NOT_FOUND":
+					setMessage({
+						type: "error",
+						text: "Le compte avec cet email n'existe pas.",
+					});
+					break;
 
-        case "INVALID_PASSWORD":
-          setMessage({
-            type: "error",
-            text: "Email ou mot de passe invalide.",
-          });
-          break;
+				case "INVALID_PASSWORD":
+					setMessage({
+						type: "error",
+						text: "Email ou mot de passe invalide.",
+					});
+					break;
 
-        default:
-          setMessage({ type: "error", text: "Erreur lors de la connexion." });
-      }
-    }
-  };
+				default:
+					setMessage({ type: "error", text: "Erreur lors de la connexion." });
+			}
+		}
+	};
 
-  return (
-    <div
-      className="flex flex-col items-center justify-center min-h-[80vh] bg-[#B0AFE4]"
-      style={{
-        backgroundImage: `url(${background_form})`,
-      }}
-    >
-      <div className="flex justify-center z-2 mb-[-2rem]">
-        <Link to={"/"}>
-          <img
-            className="w-16 h-16 rounded-full border-4 border-white lg:w-22 lg:h-22"
-            src={logo}
-            alt="City Guide"
-          />
-        </Link>
-      </div>
-      <div className="bg-white p-6 rounded-2xl shadow-lg w-80 lg:w-[60vh] max-h-[75vh] overflow-y-auto z-1">
-        <h2 className="text-center text-2xl font-semibold text-[#706EEB] mt-1.5 mb-6">
-          Connexion à votre compte
-        </h2>
+	return (
+		<>
+			<div
+				className="flex flex-col items-center justify-center min-h-[80vh] bg-[#B0AFE4]"
+				style={{
+					backgroundImage: `url(${background_form})`,
+				}}
+			>
+				<div className="flex justify-center z-2 mb-[-2rem]">
+					<Link to={"/"}>
+						<img
+							className="w-16 h-16 rounded-full border-4 border-white lg:w-22 lg:h-22"
+							src={logo}
+							alt="City Guide"
+						/>
+					</Link>
+				</div>
+				<div className="bg-white p-6 rounded-2xl shadow-lg w-80 lg:w-[60vh] max-h-[75vh] overflow-y-auto z-1">
+					<h2 className="text-center text-2xl font-semibold text-[#706EEB] mt-1.5 mb-6">
+						Connexion à votre compte
+					</h2>
 
-        <div className="flex flex-col justify-center items-center mb-8">
-          <img
-            className="w-30 h-30 rounded-full lg:w-42 lg:h-42"
-            src={connexion}
-            alt="City Guide connexion"
-          />
-          <p className="italic text-[#B0AFE4] text-sm lg:text-lg">
-            "Découvrez les secrets de votre ville"
-          </p>
-        </div>
+					<div className="flex flex-col justify-center items-center mb-8">
+						<img
+							className="w-30 h-30 rounded-full lg:w-42 lg:h-42"
+							src={connexion}
+							alt="City Guide connexion"
+						/>
+						<p className="italic text-[#B0AFE4] text-sm lg:text-lg">
+							"Découvrez les secrets de votre ville"
+						</p>
+					</div>
 
 					<form
 						onSubmit={handleLogin}
@@ -150,7 +151,7 @@ const Login = () => {
 							className={`w-full max-w-[150px] text-white py-1 rounded-[25px] bg-[#706eeb] hover:bg-[#b0afe4] ${message ? "mt-0" : "mt-5"
 								}`}
 							type="submit"
-							style={{marginBottom: message ? "0" : "20px"}}
+							style={{ marginBottom: message ? "0" : "20px" }}
 						>
 							Me connecter
 						</button>
