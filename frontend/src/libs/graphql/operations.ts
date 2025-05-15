1d3dc2443fcc89e2f5b7abc17933b26bb2c01576
bechir-elya <bechir.elya@gmail.com>
2025-05-15 14:57:54 +0200

    creating category page with adding, updating and deleting

--- frontend/src/libs/graphql/operations.ts
+++ frontend/src/libs/graphql/operations.ts
@@ -101,3 +101,52 @@ export const REGISTER_USER = gql`
   mutation RegisterUser($data: NewUserInput!) {
     registerUser(data: $data)
 }`
+
+
+export const GET_CATEGORIES = gql`
+query GetCategories {
+  getCategories {
+    id
+    name
+    description
+    color
+  }
+}`;
+
+
+export const GET_CATEGORY_BY_ID = gql`
+query GetCategoryById($categoryId: String!) {
+  getCategoryById(categoryId: $categoryId) {
+    id
+    name
+    description
+    color
+  }
+}`;
+
+
+export const CREATE_CATEGORY = gql`
+mutation CreateCategory($data: CategoryInput!) {
+  createCategory(data: $data) {
+    name
+    description
+    color
+  }
+}`;
+
+
+export const REPLACE_CATEGORY_BY_ID = gql`
+mutation ReplaceCategoryById($data: UpdateCategoryInput!, $categoryId: String!) {
+  replaceCategoryById(data: $data, categoryId: $categoryId) {
+    id
+    name
+    description
+    color
+  }
+}`;
+
+
+export const DELETE_CATEGORY = gql`
+mutation DeleteCategoryById($categoryId: String!) {
+  deleteCategoryById(categoryId: $categoryId)
+}`;
