0e2f84380f0d64ef1afb81767e71d7adc08a60ac
Noëllie CHATAIN SIX <noellie.chatain@hotmail.com>
2025-06-10 15:50:23 +0200

    fix error 'missing type' for button

--- frontend/src/pages/AdminDashboard/AdminDashboard.tsx
+++ frontend/src/pages/AdminDashboard/AdminDashboard.tsx
@@ -7,58 +7,61 @@ import CategoryManager from "../CategoryManager/CategoryManager";
 // Ici, on suppose que le rôle de l'utilisateur est "superAdmin", "adminVille", ou "user"
 // Si l'utilisateur est "user" il verra une page blanche
 export default function Dashboard({ userRole }: { userRole: string }) {
-  // État pour suivre le bouton cliqué et afficher le bon élément en conséquence
-  const [activeComponent, setActiveComponent] = useState<string | null>(null);
+	// État pour suivre le bouton cliqué et afficher le bon élément en conséquence
+	const [activeComponent, setActiveComponent] = useState<string | null>(null);
 
-  // Fonction pour afficher le composant en fonction du bouton sélectionné
-  const handleClick = (component: string) => {
-    setActiveComponent(component);
-  };
+	// Fonction pour afficher le composant en fonction du bouton sélectionné
+	const handleClick = (component: string) => {
+		setActiveComponent(component);
+	};
 
-  return (
-    <div className="w-full">
-      <span className="block mb-4">Administrateur</span>
-      <div className="w-full">
-        <div className="flex justify-between w-full">
-          {/* Onglet Ville : visible pour tous les admins, mais seulement accessible pour adminVille et superAdmin */}
-          {(userRole === "superAdmin" || userRole === "adminVille") && (
-            <button
-              onClick={() => handleClick("ville")}
-              className="text-black hover:text-gray-700 border-gray-300 px-3 py-2 text-sm font-medium last:border-none"
-            >
-              Ville
-            </button>
-          )}
+	return (
+		<div className="w-full">
+			<span className="block mb-4">Administrateur</span>
+			<div className="w-full">
+				<div className="flex justify-between w-full">
+					{/* Onglet Ville : visible pour tous les admins, mais seulement accessible pour adminVille et superAdmin */}
+					{(userRole === "superAdmin" || userRole === "adminVille") && (
+						<button
+							type="button"
+							onClick={() => handleClick("ville")}
+							className="text-black hover:text-gray-700 border-gray-300 px-3 py-2 text-sm font-medium last:border-none"
+						>
+							Ville
+						</button>
+					)}
 
-          {/* Onglet Catégorie : visible uniquement pour superAdmin */}
-          {userRole === "superAdmin" && (
-            <button
-              onClick={() => handleClick("categorie")}
-              className="text-black hover:text-gray-700 border-gray-300 px-3 py-2 text-sm font-medium last:border-none"
-            >
-              Catégorie
-            </button>
-          )}
+					{/* Onglet Catégorie : visible uniquement pour superAdmin */}
+					{userRole === "superAdmin" && (
+						<button
+							type="button"
+							onClick={() => handleClick("categorie")}
+							className="text-black hover:text-gray-700 border-gray-300 px-3 py-2 text-sm font-medium last:border-none"
+						>
+							Catégorie
+						</button>
+					)}
 
-          {/* Onglet Utilisateur : visible uniquement pour superAdmin */}
-          {(userRole === "superAdmin" || userRole === "adminVille") && (
-            <button
-              onClick={() => handleClick("utilisateur")}
-              className="text-black hover:text-gray-700 last:border-none px-3 py-2 text-sm font-medium"
-            >
-              Utilisateur
-            </button>
-          )}
-        </div>
-      </div>
+					{/* Onglet Utilisateur : visible uniquement pour superAdmin */}
+					{(userRole === "superAdmin" || userRole === "adminVille") && (
+						<button
+							type="button"
+							onClick={() => handleClick("utilisateur")}
+							className="text-black hover:text-gray-700 last:border-none px-3 py-2 text-sm font-medium"
+						>
+							Utilisateur
+						</button>
+					)}
+				</div>
+			</div>
 
-      <div className="mt-6">
-        {activeComponent === "ville" && <VilleComponent />}
+			<div className="mt-6">
+				{activeComponent === "ville" && <VilleComponent />}
 
-        {activeComponent === "categorie" && <CategoryManager />}
+				{activeComponent === "categorie" && <CategoryManager />}
 
-        {activeComponent === "utilisateur" && <UserManager />}
-      </div>
-    </div>
-  );
+				{activeComponent === "utilisateur" && <UserManager />}
+			</div>
+		</div>
+	);
 }
