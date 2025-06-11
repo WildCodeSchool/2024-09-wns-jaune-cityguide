27c4ec472f85f7f8199555009944a9f158019111
Noëllie CHATAIN SIX <noellie.chatain@hotmail.com>
2025-06-11 12:38:21 +0200

    fix footer display on mobile version

--- frontend/src/organisms/Footer.tsx
+++ frontend/src/organisms/Footer.tsx
@@ -5,58 +5,49 @@ import linkedin from "../assets/linkedin.svg";
 import instagram from "../assets/instagram.svg";
 
 const Footer: FC = () => {
-	// How to hide a component based on the url: https://medium.com/@PikoCanFly/react-js-conditionally-hide-components-according-the-current-relative-url-6012a187794a
 	const location = useLocation();
 	const isMapPage = location.pathname === "/map";
+
 	return (
 		<footer
-			className="bg-[rgb(112,110,235)] mt-auto"
+			className="bg-[rgb(112,110,235)] mt-auto text-white"
 			style={{ display: isMapPage ? "none" : "block" }}
 		>
-			<div className="container mx-auto lg:max-w-screen-xl md:max-w-screen-md py-9 px-4">
-				<div className="flex flex-col sm:flex-row justify-between items-center h-full gap-y-6 sm:gap-y-0">
-					<div className="flex gap-8">
+			<div className="container mx-auto lg:max-w-screen-xl md:max-w-screen-md py-4 px-3 sm:py-9 sm:px-4">
+				<div className="flex flex-col sm:flex-row justify-between items-center gap-y-4 sm:gap-y-0">
+					<div className="flex gap-4">
 						<Link
 							to="#"
-							className="group bg-white hover:bg-primary rounded-full shadow-xl p-3"
+							className="bg-white hover:bg-primary rounded-full shadow-md p-1.5 transition-all"
 						>
-							<img src={twitter} alt="twitter" width={18} />
+							<img src={twitter} alt="twitter" width={14} />
 						</Link>
 						<Link
 							to="#"
-							className="group bg-white hover:bg-primary rounded-full shadow-xl p-3"
+							className="bg-white hover:bg-primary rounded-full shadow-md p-1.5 transition-all"
 						>
-							<img src={instagram} alt="instagram" width={18} />
+							<img src={instagram} alt="instagram" width={14} />
 						</Link>
 						<Link
 							to="#"
-							className="group bg-white hover:bg-primary rounded-full shadow-xl p-3"
+							className="bg-white hover:bg-primary rounded-full shadow-md p-1.5 transition-all"
 						>
-							<img src={linkedin} alt="linkedIn" width={18} />
+							<img src={linkedin} alt="linkedIn" width={14} />
 						</Link>
 					</div>
 
-					<p className="text-xs font-medium text-grey dark:text-white/50 text-center my-0">
+					<p className="hidden sm:block text-xs font-medium text-white/80 text-center">
 						© 2025 - CityGuide
 					</p>
 
-					<div className="flex flex-wrap justify-center gap-6">
-						<Link
-							to="#"
-							className="text-black/70 dark:text-white/70 hover:text-primary dark:hover:text-primary text-base"
-						>
+					<div className="flex flex-row gap-4 text-[12px] sm:text-base items-center">
+						<Link to="#" className="text-white/80 hover:text-white transition">
 							Mentions légales
 						</Link>
-						<Link
-							to="#"
-							className="text-black/70 dark:text-white/70 hover:text-primary dark:hover:text-primary text-base"
-						>
+						<Link to="#" className="text-white/80 hover:text-white transition">
 							À propos
 						</Link>
-						<Link
-							to="#"
-							className="text-black/70 dark:text-white/70 hover:text-primary dark:hover:text-primary text-base"
-						>
+						<Link to="#" className="text-white/80 hover:text-white transition">
 							Contact
 						</Link>
 					</div>
