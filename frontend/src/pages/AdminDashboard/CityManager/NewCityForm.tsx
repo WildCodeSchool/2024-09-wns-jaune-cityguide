c2b06dc78bb0e9d3afd9fca5c3a3f6788343d720
Noëllie CHATAIN SIX <noellie.chatain@hotmail.com>
2025-06-12 12:01:02 +0200

    implement auto complete in search field

--- frontend/src/pages/AdminDashboard/CityManager/NewCityForm.tsx
+++ frontend/src/pages/AdminDashboard/CityManager/NewCityForm.tsx
@@ -1,23 +1,107 @@
+import { useEffect, useRef, useState } from "react";
+
+interface APIResult {
+	fulltext: string;
+	names: string[];
+	zipcode?: string;
+	x: number;
+	y: number;
+}
+
 interface NewCityFormProps {
 	onCancel: () => void;
 }
 
 export function NewCityForm({ onCancel }: NewCityFormProps) {
+	const [userInput, setUserInput] = useState<string>("");
+	const [suggestions, setSuggestions] = useState<APIResult[]>([]);
+	const [dropdownIsOpen, setDropdownIsOpen] = useState<boolean>(false);
+	const [selectedCity, setSelectedCity] = useState<APIResult | null>(null);
+
+	const isFromSelectionRef = useRef(false);
+
+	useEffect(() => {
+		if (userInput.length < 3 || isFromSelectionRef.current) {
+			setSuggestions([]);
+			setDropdownIsOpen(false);
+			isFromSelectionRef.current = false;
+			return;
+		}
+
+		const fetchSuggestions = async () => {
+			console.log("Fetching suggestions for:", userInput);
+			try {
+				const response = await fetch(
+					`https://data.geopf.fr/geocodage/completion/?text=${
+						userInput
+					}&terr=METROPOLE&poiType=administratif&type=PositionOfInterest&maximumResponses=10`,
+				);
+				const data = await response.json();
+				console.log("Data:", data);
+				if (data?.results) {
+					setSuggestions(data.results);
+					setDropdownIsOpen(true);
+				}
+			} catch (error) {
+				if (error instanceof Error) {
+					console.error("Error fetching suggestions:", error.message);
+				} else {
+					console.error("Unknown error fetching suggestions:", error);
+				}
+			}
+		};
+		const debounce = setTimeout(fetchSuggestions, 300);
+		return () => {
+			clearTimeout(debounce);
+		};
+	}, [userInput]);
+
+	const handleSelect = (city: APIResult) => {
+		isFromSelectionRef.current = true;
+		setSelectedCity(city);
+		setUserInput(city.fulltext);
+		setSuggestions([]);
+		setDropdownIsOpen(false);
+	};
+
 	return (
 		<form className="new-city-form flex flex-col w-full space-y-3">
 			<div className="form-header flex w-full items-center justify-center p-4">
 				<h3 className="text-2xl font-medium">Ajouter une ville</h3>
 			</div>
 			<div className="form-body flex flex-col space-y-4">
-				<div className="form-group flex flex-col space-y-2">
+				<div className="form-group relative flex flex-col space-y-2">
 					<label htmlFor="search">Rechercher par nom ou code postal</label>
 					<input
 						id="search"
 						name="search"
 						type="text"
+						value={userInput}
 						className="border border-[#706eeb] rounded-sm w-full px-4 py-2 placeholder:text-sm"
 						placeholder="Rechercher..."
+						autoComplete="off"
+						onChange={(e) => setUserInput(e.target.value)}
 					/>
+					{dropdownIsOpen && suggestions.length > 0 && (
+						<ul className="absolute top-full left-0 right-0 bg-white border border-gray-300 z-10 max-h-60 overflow-auto">
+							{suggestions.map((city) => (
+								<li
+									key={city.fulltext}
+									className="cursor-pointer px-4 py-2 hover:bg-[#f1f1f1]"
+									onClick={() => handleSelect(city)}
+									onKeyDown={(e) => {
+										if (e.key === "Enter" || e.key === " ") {
+											handleSelect(city);
+										}
+									}}
+								>
+									<button type="button" onClick={() => handleSelect(city)}>
+										{city.fulltext}
+									</button>
+								</li>
+							))}
+						</ul>
+					)}
 				</div>
 				<div className="form-group flex flex-col space-y-2">
 					<label htmlFor="name">Ville</label>
@@ -27,6 +111,7 @@ export function NewCityForm({ onCancel }: NewCityFormProps) {
 						type="text"
 						placeholder="Nom de la ville"
 						disabled
+						value={selectedCity?.names[0] ?? ""}
 						className="border border-[#706eeb] bg-gray-100 rounded-sm w-full px-4 py-2 placeholder:text-sm"
 					/>
 				</div>
@@ -39,6 +124,7 @@ export function NewCityForm({ onCancel }: NewCityFormProps) {
 						type="text"
 						placeholder="Code postal"
 						disabled
+						value={selectedCity?.zipcode ?? ""}
 						className="border border-[#706eeb] bg-gray-100 rounded-sm w-full px-4 py-2 placeholder:text-sm"
 					/>
 				</div>
@@ -50,6 +136,7 @@ export function NewCityForm({ onCancel }: NewCityFormProps) {
 							name="latitude"
 							type="text"
 							disabled
+							value={selectedCity?.y ?? ""}
 							placeholder="Latitude"
 							className="border border-[#706eeb] bg-gray-100 rounded-sm w-full px-4 py-2 placeholder:text-sm"
 						/>
@@ -62,6 +149,7 @@ export function NewCityForm({ onCancel }: NewCityFormProps) {
 							type="text"
 							disabled
 							placeholder="Longitude"
+							value={selectedCity?.x ?? ""}
 							className="border border-[#706eeb] bg-gray-100 rounded-sm w-full px-4 py-2 placeholder:text-sm"
 						/>
 					</div>
