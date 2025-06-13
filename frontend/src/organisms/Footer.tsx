de5c01a4e41b4e8f0b399d499ae2a099f1c19d90
bechir-elya <163391565+bechir-elya@users.noreply.github.com>
2025-06-13 17:09:39 +0200

    contact page creation (#71)

--- frontend/src/organisms/Footer.tsx
+++ frontend/src/organisms/Footer.tsx
@@ -47,7 +47,7 @@ const Footer: FC = () => {
 						<Link to="/about" className="text-white/80 hover:text-white transition">
 							À propos
 						</Link>
-						<Link to="#" className="text-white/80 hover:text-white transition">
+						<Link to="/contact" className="text-white/80 hover:text-white transition">
 							Contact
 						</Link>
 					</div>
