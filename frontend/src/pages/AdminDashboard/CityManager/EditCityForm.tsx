b3768ef5859a786d4395af23e7edfce541df38a3
Noëllie CHATAIN SIX <noellie.chatain@hotmail.com>
2025-07-07 17:18:29 +0200

    update delete button

--- frontend/src/pages/AdminDashboard/CityManager/EditCityForm.tsx
+++ frontend/src/pages/AdminDashboard/CityManager/EditCityForm.tsx
@@ -151,9 +151,24 @@ export function EditCityForm({ city }: EditFormProps) {
 									<button
 										type="button"
 										onClick={() => handleAssignUserRole(admin.id)}
-										className="text-red-500 hover:text-red-700"
+										className="text-red-500 hover:text-red-700 hover:cursor-pointer"
 									>
-										X
+										<svg
+											xmlns="http://www.w3.org/2000/svg"
+											width="24"
+											height="24"
+											viewBox="0 0 24 24"
+											fill="none"
+											stroke="currentColor"
+											strokeWidth="2"
+											strokeLinecap="round"
+											strokeLinejoin="round"
+											className=""
+										>
+											<title>Fermer</title>
+											<path d="M18 6 6 18" />
+											<path d="m6 6 12 12" />
+										</svg>
 									</button>
 								</div>
 							))
