7ec0f776ba3ab33c0e06436e0d9ad57d229d5b81
Noëllie CHATAIN SIX <noellie.chatain@hotmail.com>
2025-07-18 10:11:17 +0200

    remove unused imports

--- frontend/src/pages/Inscription/Inscription.test.tsx
+++ frontend/src/pages/Inscription/Inscription.test.tsx
@@ -1,11 +1,5 @@
 import { describe, it, expect, vi } from "vitest";
-import {
-	act,
-	fireEvent,
-	render,
-	screen,
-	waitFor,
-} from "@testing-library/react";
+import { fireEvent, render, screen, waitFor } from "@testing-library/react";
 import "@testing-library/jest-dom";
 
 vi.mock("../../store/citiesStore.ts", () => ({
