05ea3edcd441a954198f7ed333bdac8bb2db08bc
bechir-elya <163391565+bechir-elya@users.noreply.github.com>
2025-10-21 11:33:00 +0200

    adding prod environment (#84)

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -83,8 +83,11 @@ const router = createBrowserRouter([
 	},
 ]);
 
+console.log("Base URL:", import.meta.env.VITE_BASE_URL); 
+console.log("Gateway Port:", import.meta.env.VITE_GATEWAY_PORT);
+
 export const client = new ApolloClient({
-	uri: `http://localhost:${import.meta.env.VITE_GATEWAY_PORT}/api`,
+	uri: `${import.meta.env.VITE_BASE_URL}/api`,
 	cache: new InMemoryCache(),
 	credentials: "include",
 });
