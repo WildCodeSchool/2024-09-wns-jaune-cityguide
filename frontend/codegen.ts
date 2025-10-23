341f5718be793adaafb12d05f29aefac95f65c35
Noëllie CHATAIN SIX <noellie.chatain@hotmail.com>
2025-10-23 18:52:00 +0200

    fix path to .env files (merge of PR #88 pending)

--- frontend/codegen.ts
+++ frontend/codegen.ts
@@ -1,25 +1,28 @@
-import type { CodegenConfig } from '@graphql-codegen/cli';
-import * as dotenv from 'dotenv';
+import type { CodegenConfig } from "@graphql-codegen/cli";
+import * as dotenv from "dotenv";
 
-const envFile = process.env.NODE_ENV === 'production' ? '../.env.prod' : '../.env.dev';
+const envFile =
+	process.env.NODE_ENV === "production"
+		? "../environments/prod/.env.prod"
+		: "../environments/dev/.env.dev";
 dotenv.config({ path: envFile });
 
 const config: CodegenConfig = {
-    overwrite: true,
-    schema: `${process.env.BASE_URL}/api`,
-    documents: "./src/libs/graphql/operations.ts",
-    generates: {
-        "./src/libs/graphql/generated/graphql-types.ts": {
-            plugins: [
-                "typescript",
-                "typescript-operations",
-                "typescript-react-apollo"
-            ],
-            config: {
-                withHooks: true,
-            },
-        },
-    },
+	overwrite: true,
+	schema: `${process.env.BASE_URL}/api`,
+	documents: "./src/libs/graphql/operations.ts",
+	generates: {
+		"./src/libs/graphql/generated/graphql-types.ts": {
+			plugins: [
+				"typescript",
+				"typescript-operations",
+				"typescript-react-apollo",
+			],
+			config: {
+				withHooks: true,
+			},
+		},
+	},
 };
 
 export default config;
