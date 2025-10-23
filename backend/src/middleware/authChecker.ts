b8a809e46203a9db0a877c9adb304cb9a02ce89f
Noëllie CHATAIN SIX <noellie.chatain@hotmail.com>
2025-10-23 19:06:30 +0200

    delete comments

--- backend/src/middleware/authChecker.ts
+++ backend/src/middleware/authChecker.ts
@@ -13,22 +13,10 @@ export const customAuthChecker: AuthChecker<{ user?: User }> = (
 
 	if (!currentUser) return false;
 
-	// Si aucun rôle requis : autorisation par défaut
 	if (roles.length === 0) return true;
 
 	return roles.includes(currentUser.role);
 };
-
-// export const customAuthChecker: AuthChecker<Context> = ({ context }, roles) => {
-//     const user = context.user;
-//     if (!user) { return false; // utilisateur non connecté};
-
-//     //  Si aucun rôle spécifique n'est exigé, le user est simplement authentifié
-//     if (roles.length === 0) { return true; };
-
-//     // Vérifie si le user a au moins un des rôles nécessaires
-//     return roles.includes(user.role);
-// }
 
 export function requireRole(
 	user: { id: string; role: UserRole } | undefined,
--- backend/src/resolvers/PasswordResolver.ts
+++ backend/src/resolvers/PasswordResolver.ts
@@ -88,8 +88,8 @@ export class PasswordResolver {
 
     const hashedPassword = await argon.hash(newPassword);
     user.hashedPassword = hashedPassword;
-    user.resetToken = null; // Clear the reset token
-    user.resetTokenExpiration = null; // Clear the expiration date
+    user.resetToken = null;
+    user.resetTokenExpiration = null;
     await user.save();
 
     return "Mot de passe réinitialisé avec succès";
